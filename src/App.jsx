94f9ba3667e959969dea8dae66cd7c1e71a94245
Dario Urdaneta <dariourdaneta@gmail.com>
2023-10-13 18:25:16 -0300

    fixing buscador bug, rendering filter data

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([...BaseColaboradores]);
+  const [colaboradoresFiltrados, setColaboradoresFiltrados] = useState([...BaseColaboradores]);
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
 
@@ -19,12 +20,21 @@ function App() {
     const updatedColaboradores = [...colaboradores];
     updatedColaboradores.splice(index, 1);
     setColaboradores(updatedColaboradores);
+    setColaboradoresFiltrados(updatedColaboradores); // Actualiza colaboradoresFiltrados después de la eliminación
+
   };
 
   const handleAgregarColaborador = (nuevoColaborador) => {
     setColaboradores([...colaboradores, nuevoColaborador]);
+    setColaboradoresFiltrados([...colaboradores, nuevoColaborador]); // Actualiza colaboradoresFiltrados después de la adición
     setSuccess(true)
-
+  };
+  
+  const handleBuscar = (term) => {
+    const filteredColaboradores = colaboradores.filter((colaborador) => {
+      return colaborador.nombre.toLowerCase().includes(term.toLowerCase());
+    });
+    setColaboradoresFiltrados(filteredColaboradores);
   };
 
   return (
@@ -32,8 +42,10 @@ function App() {
       <div className="row justify-content-center">
         <div className="col-md-8">
           <div className="d-flex flex-column">
-            <Buscador />
-            <Listado colaboradores={colaboradores} handleDelete={handleDelete} />
+            <Buscador
+            onBuscar={ handleBuscar }
+            />
+            <Listado colaboradores={colaboradoresFiltrados} handleDelete={handleDelete} />
           </div>
         </div>
         <div className="col-md-4">
