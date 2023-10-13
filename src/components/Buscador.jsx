21b9db161dd459e35abaaebfdb5163efe6f3b847
Dario Urdaneta <dariourdaneta@gmail.com>
2023-10-13 18:10:48 -0300

    fixing Buscador component

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -3,44 +3,32 @@ import PropTypes from 'prop-types'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-function Buscador({ colaboradores, setColaboradores, filterColaboradores }) {
-    const [searchTerm, setSearchTerm] = useState('');
+function Buscador({ onBuscar }) {
+    const [term, setTerm] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setColaboradores(filterColaboradores(colaboradores, searchTerm));
+    const handleInputChange = (e) => {
+    setTerm(e.target.value);
+    onBuscar(e.target.value);
     };
 
     return (
-    <Form onSubmit={handleSubmit}>
     <InputGroup className="mb-3 mt-3 w-50 p-2">
     <InputGroup.Text id="basic-addon1">
             <i className="fa-solid fa-magnifying-glass"></i>
         </InputGroup.Text>
         <Form.Control
             type="text"
+            className="form-control"
             placeholder="Buscar colaboradores"
-            value={searchTerm}
-            onChange={(event) => setSearchTerm(event.target.value)} // Puedes usar setSearchTerm directamente aquí
+            value={ term } 
+            onChange={ handleInputChange }
         />
         </InputGroup>
-    </Form>
 );
 }
 
 Buscador.propTypes = {
-    colaboradores: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            nombre: PropTypes.string.isRequired,
-            correo: PropTypes.string.isRequired,
-            edad: PropTypes.number.isRequired,
-            cargo: PropTypes.string.isRequired,
-            telefono: PropTypes.string.isRequired,
-        })
-    ).isRequired,
-    setColaboradores: PropTypes.func.isRequired,
-    filterColaboradores: PropTypes.func.isRequired,
+    onBuscar: PropTypes.string
 };
 
 export default Buscador;
